7690f4ffa2bdaec4e121e85409ab6f509b149741
villiamks <villiamks@gmail.com>
2025-03-03 12:51:43 +0100

    Task 1

--- Solarsystem_and_planets/Program.cs
+++ Solarsystem_and_planets/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Solarsystem_and_planets;
+
+class Astronomy
+{
+    public static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject>
+        {
+            new Star("Sun"),
+            new Planet("Mercury"),
+            new Planet("Venus"),
+            new Planet("Terra"),
+            new Moon("The Moon")
+        };
+        foreach (SpaceObject obj in solarSystem)
+        {
+            obj.Draw();
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Solarsystem_and_planets/Spaceobject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Solarsystem_and_planets
+{
+    public class SpaceObject
+    {
+        public String Name { get; protected set; }
+        public SpaceObject(String name)
+        {
+            Name = name;
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    public class Star : SpaceObject
+    {
+        public Star(String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Star : ");
+            base.Draw();
+        }
+    }
+    public class Planet : SpaceObject
+    {
+        public Planet(String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+    public class Moon : Planet
+    {
+        public Moon(String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Moon : ");
+            base.Draw();
+        }
+    }
+}
